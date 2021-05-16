676c222e00a2c41ea2ef0824714555051a70514d
yunusemreyilmazz <36795649+yunusemreyilmazz@users.noreply.github.com>
2021-05-16 15:20:04 +0300

    Creating Product Class
    We initialized product attributes.

---
+++ src/main/java/com/example/team7CNG457webservice/entity/Product.java
@@ -0,0 +1,33 @@
+package com.example.team7CNG457webservice.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.List;
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "Products")
+public class Product {
+    @Id
+    @Column(name = "ProductID",nullable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int product_id;
+
+    private String brand;
+    private String model;
+    private int screenSize;
+    private int price;
+
+
+
+    @OneToMany(mappedBy = "product", cascade = CascadeType.ALL)
+    List<Computer> computers;
+
+    @OneToMany(mappedBy = "product", cascade = CascadeType.ALL)
+    List<Phone> phones;
+
+}
