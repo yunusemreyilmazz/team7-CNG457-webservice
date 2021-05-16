6f7a35d78c22b5b8b07e275a000d35ca6979157d
yunusemreyilmazz <36795649+yunusemreyilmazz@users.noreply.github.com>
2021-05-16 14:27:06 +0300

    Initial commit

---
+++ src/main/java/com/example/team7CNG457webservice/Team7Cng457WebserviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.team7CNG457webservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Team7Cng457WebserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Team7Cng457WebserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/team7CNG457webservice/Team7Cng457WebserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.team7CNG457webservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Team7Cng457WebserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
