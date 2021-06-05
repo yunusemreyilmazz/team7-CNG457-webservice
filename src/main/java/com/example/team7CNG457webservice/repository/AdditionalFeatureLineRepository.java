478089fd635521a4c6af67c2f536cad1d221af32
yunusemreyilmazz <36795649+yunusemreyilmazz@users.noreply.github.com>
2021-06-05 23:09:59 +0300

    Adding AdditionalFeatureLineController service and repository.
    This is the weak  table version of many to many relation between product and additional feature.

---
+++ src/main/java/com/example/team7CNG457webservice/repository/AdditionalFeatureLineRepository.java
@@ -0,0 +1,8 @@
+package com.example.team7CNG457webservice.repository;
+
+import com.example.team7CNG457webservice.entity.AdditionalFeatureLine;
+import com.example.team7CNG457webservice.entity.AfId;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AdditionalFeatureLineRepository extends JpaRepository<AdditionalFeatureLine, AfId> {
+}
